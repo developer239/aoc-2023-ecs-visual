3168ce479dac71a3d241db3cefd3860b2cd538b5
michaljarnot <m.jarnot@yahoo.com>
2023-12-03 13:34:06 +0100

    feat: make bboxes more visible

--- src/apps/day3/src/systems/RenderColliderSystem.h
+++ src/apps/day3/src/systems/RenderColliderSystem.h
@@ -17,17 +17,13 @@ class RenderCollidersSystem : public ECS::System {
   }
 
   void Render(Core::Renderer& renderer) {
+    const int borderWidth = 5;
+
     for (auto entity : GetSystemEntities()) {
       auto& boxCollider =
           ECS::Registry::Instance().GetComponent<BoxColliderComponent>(entity);
       auto& rigidBody =
           ECS::Registry::Instance().GetComponent<RigidBodyComponent>(entity);
-
-      SDL_Rect colliderRect = {
-          static_cast<int>(rigidBody.position.x + boxCollider.offset.x),
-          static_cast<int>(rigidBody.position.y + boxCollider.offset.y),
-          boxCollider.width,
-          boxCollider.height};
 
       SDL_SetRenderDrawColor(
           renderer.Get().get(),
@@ -36,15 +32,17 @@ class RenderCollidersSystem : public ECS::System {
           boxCollider.color.b,
           boxCollider.color.a
       );
-      SDL_RenderDrawRect(renderer.Get().get(), &colliderRect);
+
+      for (int i = 0; i < borderWidth; ++i) {
+        SDL_Rect colliderRect = {
+            static_cast<int>(rigidBody.position.x + boxCollider.offset.x - i),
+            static_cast<int>(rigidBody.position.y + boxCollider.offset.y - i),
+            boxCollider.width + i * 2,
+            boxCollider.height + i * 2};
+        SDL_RenderDrawRect(renderer.Get().get(), &colliderRect);
+      }
     }
 
-    SDL_SetRenderDrawColor(
-        renderer.Get().get(),
-        0,
-        0,
-        0,
-        255
-    );
+    SDL_SetRenderDrawColor(renderer.Get().get(), 0, 0, 0, 255);
   }
 };
